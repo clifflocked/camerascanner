ab9fa5ee577fe7babcb90e79ed6fafd3bbc96329
Segen Stoutamire <72527185+clifflocked@users.noreply.github.com>
2025-01-11 11:45:05 -0800

    Initial commit

---
+++ scanner.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"	
+	"log"
+)
+
+func main() {
+	out, err := exec.Command("ls", "/dev/v4l/by-id/", "|", "grep", "video0").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	devices := strings.Split(string(out), "\n ")
+	var sNums []string
+	serial := ""
+
+	for _, e := range devices {
+		start := strings.Index(e, "Camera_")
+		for j := 0; j < 10; j++ {
+			if string(e[start + j]) == "-" {
+				sNums = append(sNums, serial)
+				serial = ""
+				break
+			} else {
+				serial += string(e[start+j])
+			}
+		}
+	}
+	fmt.Print(sNums)
+
+}
